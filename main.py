2160b719253061ef861cfe225198ea3f5d2d6828
72PikaChu72 <72vladvlad72@gmail.com>
2024-04-06 23:44:07 +0500

    Добавил поток видео

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ Session(app)
 @app.get('/') # Маршрут на первоначальную страницу
 async def index(request):
     user = request.ctx.session.get('user_id')
-    print(user)
-    request.ctx.session['user_id'] = 123
     data = {}
     template = env.get_template('index.html') #Получили шаблон
     return response.html(template.render(data=data))
@@ -42,7 +40,40 @@ async def get_posts(request):
         return json({'posts':Database.getAllPosts()}, status=200)
     return json({'posts':Database.getPostsForUser(user_id)}, status=200)
 
-@app.route('/images/<image>')
+@app.get('/about')
+async def about(request):
+    data = {}
+    template = env.get_template('about.html') #Получили шаблон
+    return response.html(template.render(data=data))
+
+@app.route('/servevideo/<filename:str>')
+async def serve_video(request, filename):
+    video_path = './static/video/' + filename
+    # Открываем файл видео
+    with open(video_path, 'rb') as video_file:
+        video_data = video_file.read()
+
+    headers = {'Accept-Ranges': 'bytes'}
+    content_range = request.headers.get('Range')
+
+    if content_range:
+        # Разбираем значение Range заголовка
+        start, end = content_range.replace('bytes=', '').split('-')
+        start = int(start)
+        end = int(end) if end else len(video_data) - 1
+
+        # Определяем длину контента и формируем заголовок Content-Range
+        content_length = end - start + 1
+        headers['Content-Range'] = f'bytes {start}-{end}/{len(video_data)}'
+        
+        # Вырезаем запрошенный диапазон данных из файла
+        video_chunk = video_data[start:end+1]
+        return response.raw(video_chunk, headers=headers, status=206)
+
+    # Если Range не указан, отправляем весь файл
+    return await response.file_stream(video_path, headers=headers)
+
+@app.route('/images/<image:str>')
 async def serve_image(request, image):
     return response.file('./static/images/' + image)
 
